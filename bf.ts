f150b737ba518acd3389b353b38a49e70008578e
cwd-k2 <29105991+cwd-k2@users.noreply.github.com>
2025-04-08 18:32:29 +0900

    skip/back don't require other than tapepg

--- bf.ts
+++ bf.ts
@@ -48,46 +48,23 @@ type MakePg<P extends string> =
 type Init<
   P extends string,
   I extends string
-> = Runner<
-  MakeMm<9>, // too small but for the limitations
-  MakePg<P>,
-  I,
-  ""
->;
+> = Runner<MakeMm<9>, MakePg<P>, I, "">;
 
 // prettier-ignore
-type Skip<R, N extends number> =
-  R extends Runner<
-    infer M extends TapeMm,
-    infer P extends TapePg,
-    infer I extends string,
-    infer O extends string
-  >
-    ? P["c"] extends "]"
-      ? N extends 0
-        ? R
-        : Skip<Runner<M, Next<P>, I, O>, DecrementMap[N]>
-    : P["c"] extends "["
-      ? Skip<Runner<M, Next<P>, I, O>, IncrementMap[N]>
-    : /* else */ Skip<Runner<M, Next<P>, I, O>, N>
-  : never;
+type Skip<P extends TapePg, N extends number = 0>
+  = P['c'] extends ']'
+    ? N extends 0 ? P : Skip<Next<P>, DecrementMap[N]>
+  : P['c'] extends '['
+    ? Skip<Next<P>, IncrementMap[N]>
+  : Skip<Next<P>, N>;
 
 // prettier-ignore
-type Back<R, N extends number> =
-  R extends Runner<
-    infer M extends TapeMm,
-    infer P extends TapePg,
-    infer I extends string,
-    infer O extends string
-  >
-    ? P["c"] extends "["
-      ? N extends 0
-        ? R
-        : Back<Runner<M, Prev<P>, I, O>, DecrementMap[N]>
-    : P["c"] extends "]"
-      ? Back<Runner<M, Prev<P>, I, O>, IncrementMap[N]>
-    : /* else */ Back<Runner<M, Prev<P>, I, O>, N>
-  : never;
+type Back<P extends TapePg, N extends number = 0>
+  = P["c"] extends "["
+    ? N extends 0 ? P : Back<Prev<P>, DecrementMap[N]>
+  : P["c"] extends "]"
+    ? Back<Prev<P>, IncrementMap[N]>
+  : Back<Prev<P>, N>;
 
 // prettier-ignore
 type Step<R> =
@@ -101,21 +78,13 @@ type Step<R> =
     : P['c'] extends "-" ? Runner<Decr<M>, Next<P>, I, O>
     : P['c'] extends ">" ? Runner<Next<M>, Next<P>, I, O>
     : P['c'] extends "<" ? Runner<Prev<M>, Next<P>, I, O>
-    : P['c'] extends "["
-      ? M['c'] extends 0
-        ? Skip<Runner<M, Next<P>, I, O>, 0>
-        : Runner<M, Next<P>, I, O>
-    : P['c'] extends "]"
-      ? M['c'] extends 0
-        ? Runner<M, Next<P>, I, O>
-        : Back<Runner<M, Prev<P>, I, O>, 0>
+    : P['c'] extends "[" ? Runner<M, M['c'] extends 0 ? Skip<Next<P>> : Next<P>, I, O>
+    : P['c'] extends "]" ? Runner<M, M['c'] extends 0 ? Next<P> : Back<Prev<P>>, I, O>
     : P['c'] extends "."
       ? Runner<M, Next<P>, I, `${O}${NumToCharMap[M['c']]}`>
     : P['c'] extends ","
-      ? I extends `${infer C}${infer S}`
-        ? Runner<PutC<M, CharToNumMap[C]>, Next<P>, S, O>
-        : never
-    : /* else */ O
+      ? I extends `${infer C}${infer S}` ? Runner<PutC<M, CharToNumMap[C]>, Next<P>, S, O> : never
+    : O /* end */
   : never;
 
 // prettier-ignore
